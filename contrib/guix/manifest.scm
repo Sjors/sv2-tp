d0a021c468fe26c6a86b9b0eab7378236cb76ee5
fanquake <fanquake@gmail.com>
2025-09-01 14:17:59 +0100

    guix: remove bison
    This was only needed for the GUI.

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -1,6 +1,5 @@
 (use-modules (gnu packages)
              ((gnu packages bash) #:select (bash-minimal))
-             (gnu packages bison)
              ((gnu packages certs) #:select (nss-certs))
              ((gnu packages cmake) #:select (cmake-minimal))
              (gnu packages commencement)
@@ -561,8 +560,7 @@ inspecting signatures in Mach-O binaries.")
                  nss-certs
                  osslsigncode))
           ((string-contains target "-linux-")
-           (list bison
-                 pkg-config
+           (list pkg-config
                  (list gcc-toolchain-13 "static")
                  (make-bitcoin-cross-toolchain target)))
           ((string-contains target "darwin")
