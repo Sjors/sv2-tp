2efb853ca165e4ed2ab53657f8ff50085ae0d7e7
fanquake <fanquake@gmail.com>
2025-12-09 11:31:16 +0000

    guix: use a temporary file over sponge
    Remove sponge (moreutils).
    
    Backport of bitcoin/bitcoin#34627.

--- contrib/guix/libexec/codesign.sh
+++ contrib/guix/libexec/codesign.sh
@@ -132,6 +132,7 @@ mv --no-target-directory "$OUTDIR" "$ACTUAL_OUTDIR" \
     || ( rm -rf "$ACTUAL_OUTDIR" && exit 1 )
 
 (
+    tmp="$(mktemp)"
     cd /outdir-base
     {
         echo "$CODESIGNING_TARBALL"
@@ -140,5 +141,6 @@ mv --no-target-directory "$OUTDIR" "$ACTUAL_OUTDIR" \
     } | xargs realpath --relative-base="$PWD" \
         | xargs sha256sum \
         | sort -k2 \
-        | sponge "$ACTUAL_OUTDIR"/SHA256SUMS.part
+        > "$tmp";
+    mv "$tmp" "$ACTUAL_OUTDIR"/SHA256SUMS.part
 )
