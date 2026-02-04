7271d0b849991b23e8599f343690dba06e0d1456
fanquake <fanquake@gmail.com>
2026-02-04 17:15:08 +0000

    guix: add --no-same-owner to TAR_OPTIONS
    So it's used to extract tarballs.
    
    Backport of bitcoin/bitcoin#34550.

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -158,7 +158,7 @@ case "$HOST" in
 esac
 
 # Environment variables for determinism
-export TAR_OPTIONS="--owner=0 --group=0 --numeric-owner --mtime='@${SOURCE_DATE_EPOCH}' --sort=name"
+export TAR_OPTIONS="--no-same-owner --owner=0 --group=0 --numeric-owner --mtime='@${SOURCE_DATE_EPOCH}' --sort=name"
 export TZ="UTC"
 
 ####################
