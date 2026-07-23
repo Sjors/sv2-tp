0ca1c30da886ba51066bd64df29c68b46305cdad
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-07-23 12:31:31 +0100

    scripted-diff: Use long form of shell options in Guix scripts
    This improves readability and consistency across all Guix scripts.
    
    -BEGIN VERIFY SCRIPT-
    
    sed -i "s/^set -e\>/set -o errexit/g" \
     $( git grep -l "set -e" ./contrib/guix )
    
    -END VERIFY SCRIPT-
    
    Backport of bitcoin/bitcoin#35775.

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -3,7 +3,7 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 export LC_ALL=C.UTF-8
-set -e -o pipefail
+set -o errexit -o pipefail
 
 # shellcheck source=setup.sh
 source "$(dirname "${BASH_SOURCE[0]}")/setup.sh"
--- contrib/guix/libexec/prelude.bash
+++ contrib/guix/libexec/prelude.bash
@@ -3,7 +3,7 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit.
 export LC_ALL=C.UTF-8
-set -e -o pipefail
+set -o errexit -o pipefail
 
 source contrib/shell/realpath.bash
 source contrib/shell/git-utils.bash
